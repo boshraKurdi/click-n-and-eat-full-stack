2231d9c209cd41920129db84f2b9586fe390dff0
boshra <boshra@gmail.com>
2024-12-17 15:07:28 +0300

    update back auth

--- back/app/Http/Controllers/AuthController.php
+++ back/app/Http/Controllers/AuthController.php
@@ -22,7 +22,7 @@ class AuthController extends Controller
         if (!$token) {
             return response()->json([
                 'status' => 'error',
-                'message' => 'Unauthorized',
+                'message' => 'unauthorized',
             ], 401);
         }
 
